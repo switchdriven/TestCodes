e9c4618e0a283a2de03ebdf40150179c6c7f738b
Junya Satoh <junya.satoh@gmail.com>
2012-02-04 17:45:37 +0900

    Init

---
+++ CodeTest01/CodeTest01/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  CodeTest01
+//
+//  Created by Junya Satoh on 12/02/04.
+//  Copyright (c) 2012年 Switch Driven. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+

c93a2eb9b7d850685b20a85e73c343203f2d3fe4
Junya Satoh <junya.satoh@gmail.com>
2012-02-04 20:32:24 +0900

    練習用のコードをのせてみる

---
+++ HelloWold/HelloWold/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  HelloWold
+//
+//  Created by Junya Satoh on 12/02/04.
+//  Copyright (c) 2012年 Switch Driven. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"日本語でOK");
+        
+    }
+    return 0;
+}
+
